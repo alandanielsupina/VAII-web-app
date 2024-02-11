a45e001ae03e7d05e976fadf33e4b2abbbfd1ae4
a3soft.supina <alan.supina@a3soft.sk>
2024-02-11 16:51:52 +0100

    Added companies table, Company model and relationship between Company and User

---
+++ database/migrations/2024_02_11_153239_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained();
+            $table->string('name');
+            $table->string('city');
+            $table->string('category');
+            $table->string('address');
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
